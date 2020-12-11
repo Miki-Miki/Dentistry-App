4c833f6c83a002b3d6e4ad15848745913e9d3e1b
SamirHehz <samir.mumic@edu.fit.ba>
2020-12-11 20:53:56 +0100

    Dodana pretraga (Lijekovi) i count lijekova

--- B.U.Z/B.U.Z/Controllers/LijekoviController.cs
+++ B.U.Z/B.U.Z/Controllers/LijekoviController.cs
@@ -4,16 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using B.U.Z.Data;
 using B.U.Z.Models;
+using B.U.Z.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace B.U.Z.Controllers
 {
     public class LijekoviController : Controller
     {
-        public IActionResult Lijekovi()
+        public IActionResult Lijekovi(string filter)
         {
             ApplicationDbContext db = new ApplicationDbContext();
-            List<Lijekovi> m = db.Lijekovi.ToList();
+            List<Lijekovi> m = db.Lijekovi.Where(s => filter == null || s.Naziv.ToLower().StartsWith(filter)).ToList();
             return View(m);
         }
         public IActionResult NoviLijek()
@@ -39,6 +40,29 @@ namespace B.U.Z.Controllers
             ApplicationDbContext db = new ApplicationDbContext();
             Lijekovi l = db.Lijekovi.Find(LijekId);
             db.Lijekovi.Remove(l);
+            db.SaveChanges();
+            return Redirect("Lijekovi");
+        }
+        public IActionResult UrediLijek(int LijekId)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            Lijekovi l = db.Lijekovi.Find(LijekId);
+            int redniBroj = db.Lijekovi.Count() + 1;
+            var m = new LijekoviUrediVM
+            {
+                Lijek_Id = l.Id,
+                Naziv = l.Naziv,
+                Opis = l.Opis,
+                RedniBroj = redniBroj
+            };
+            return View(m);
+        }
+        public IActionResult SpasiNoviStariLijek(LijekoviUrediVM l)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            Lijekovi novi = db.Lijekovi.Find(l.Lijek_Id);
+            novi.Naziv = l.Naziv;
+            novi.Opis = l.Opis;
             db.SaveChanges();
             return Redirect("Lijekovi");
         }
---
+++ B.U.Z/B.U.Z/ViewModels/LijekoviUrediVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B.U.Z.ViewModels
+{
+    public class LijekoviUrediVM
+    {
+        public int Lijek_Id { get; set; }
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public int RedniBroj { get; set; }
+    }
+}
