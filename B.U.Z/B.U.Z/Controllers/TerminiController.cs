5d2e016ac1ade69a50e19826d0368328ab9e8447
weh1d <haticv92@gmail.com>
2021-01-14 14:34:58 +0100

    Dodane osnove Pregeleda i dodavanja termina

--- B.U.Z/B.U.Z/Controllers/TerminiController.cs
+++ B.U.Z/B.U.Z/Controllers/TerminiController.cs
@@ -6,6 +6,7 @@ using B.U.Z.Data;
 using B.U.Z.Models;
 using B.U.Z.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace B.U.Z.Controllers
 {
@@ -14,16 +15,63 @@ namespace B.U.Z.Controllers
         public IActionResult Termini()
         {
             ApplicationDbContext db = new ApplicationDbContext();
-            List<Termini> termini = db.Termini.ToList(); 
+            List<Termini> termini = db.Termini.Include(s => s.Pacijent).ToList();
             return View(termini);
         }
 
-        public IActionResult DodajTermin()
+        public IActionResult DodajTermin(Termini t)
         {
             ApplicationDbContext db = new ApplicationDbContext();
-            
+            Pacijent pacijent = db.Pacijenti.Find(t.PacijentId);
 
-            return View();
+            Termini termin = new Termini
+            {
+                Pacijent = pacijent,
+                PacijentId = pacijent.Id,
+                TerminStart = t.TerminStart,
+                TerminEnd = t.TerminEnd,
+                basePrice = t.basePrice,
+            };
+            db.Termini.Add(termin);
+            db.SaveChanges();
+
+            List<Termini> termini = db.Termini.Include(s => s.Pacijent).ToList();
+            return View("Termini", termini);
         }
+
+
+        public IActionResult NoviTermin()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            List<Pacijent> pacijenti = db.Pacijenti.ToList();
+
+            return View(pacijenti);
+        }
+
+        public IActionResult OdaberiPacijenta(string pacijentId)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            Pacijent pacijent = db.Pacijenti.Find(pacijentId);
+            return View( "PacijentOdabir", pacijent);
+        }
+
+        public IActionResult OdaberiTermin(int t)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            Termini termin = db.Termini.Find(t);
+
+            Pacijent pacijent = db.Pacijenti.Find(termin.PacijentId);
+
+            TerminiVM terminVM = new TerminiVM
+            {
+                basePrice = termin.basePrice,
+                TerminStart = termin.TerminStart,
+                TerminEnd = termin.TerminEnd,
+                pacijent = pacijent
+            };
+           
+            return View("TerminOdabir", terminVM);
+        }
+   
     }
 }
---
+++ B.U.Z/B.U.Z/ViewModels/TerminiVM.cs
@@ -0,0 +1,18 @@
+﻿using B.U.Z.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B.U.Z.ViewModels
+{
+    public class TerminiVM
+    {
+        public double basePrice { get; set; }
+        public DateTime TerminStart { get; set; }
+        public DateTime TerminEnd { get; set; }
+
+        public Pacijent pacijent { get; set; }
+
+    }
+}
