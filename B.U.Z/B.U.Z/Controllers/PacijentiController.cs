fd741394c31cf7bb195ab7e31d1644f70a521d66
SamirHehz <samir.mumic@edu.fit.ba>
2021-01-15 19:49:40 +0100

    Dodani atributi za novog pacijenta

--- B.U.Z/B.U.Z/Controllers/PacijentiController.cs
+++ B.U.Z/B.U.Z/Controllers/PacijentiController.cs
@@ -1,17 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using B.U.Z.Areas.Identity.Pages.Account;
 using B.U.Z.Data;
 using B.U.Z.Models;
 using B.U.Z.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
 
 namespace B.U.Z.Controllers
 {
     public class PacijentiController : Controller
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public List<AuthenticationScheme> ExternalLogins { get; private set; }
+
+        public PacijentiController (
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager,
+        ILogger<RegisterModel> logger,
+        IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
         public IActionResult Pacijenti(string filter)
         {
             ApplicationDbContext db = new ApplicationDbContext();
@@ -22,28 +48,94 @@ namespace B.U.Z.Controllers
         {
             ApplicationDbContext db = new ApplicationDbContext();
             int UkupnoPacijenata = db.Pacijenti.Count() + 1;
+            string pass = CreatePassword(12);
             PacijentiDodajVM model = new PacijentiDodajVM
             {
                 Gradovi = db.Grad.OrderBy(a => a.Naziv).Select(a => new SelectListItem { Value = a.Id.ToString(), Text = a.Naziv }).ToList(),
                 Spolovi=db.Spol.OrderBy(a => a.Naziv).Select(a => new SelectListItem { Value = a.Id.ToString(), Text = a.Naziv }).ToList(),
-                UkupnoPacijenata=UkupnoPacijenata
+                UkupnoPacijenata=UkupnoPacijenata,
+                PasswordHash=pass
             };
             return View(model);
         }
-        public IActionResult SpasiNovogPacijenta(PacijentiDodajVM l)
+        public string CreatePassword(int length)
         {
-            ApplicationDbContext db = new ApplicationDbContext();
-            Pacijent novi = new Pacijent
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$%&/()=?*~";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
             {
-               FirstName=l.Ime,
-               LastName=l.Prezime,
-               GodinaRodjenja=l.DatumRodjenja,
-               PhoneNumber=l.BrojTelefona,
-               SpolId=l.SpolId,
-               GradId=l.GradId
-            };
-            db.Pacijenti.Add(novi);
-            db.SaveChanges();
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+        //public IActionResult SpasiNovogPacijenta(PacijentiDodajVM l)
+        //{
+        //    ApplicationDbContext db = new ApplicationDbContext();
+        //    Pacijent novi = new Pacijent
+        //    {
+        //       FirstName=l.Ime,
+        //       LastName=l.Prezime,
+        //       GodinaRodjenja=l.DatumRodjenja,
+        //       PhoneNumber=l.BrojTelefona,
+        //       SpolId=l.SpolId,
+        //       GradId=l.GradId
+        //    };
+        //    db.Pacijenti.Add(novi);
+        //    db.SaveChanges();
+        //    return Redirect("Pacijenti");
+        //}
+        public async Task<IActionResult> SpasiNovogPacijenta(PacijentiDodajVM s, string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var user = new Pacijent
+                {
+                    UserName = s.BrojTelefona,
+                    FirstName = s.Ime,
+                    PasswordHash=s.PasswordHash,
+                    LastName = s.Prezime,
+                    PhoneNumber=s.BrojTelefona,
+                    SpolId = s.SpolId,
+                    GradId = s.GradId, 
+                };
+                var result = await _userManager.CreateAsync(user, s.PasswordHash);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    //await _emailSender.SendEmailAsync(System.Windows.Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        //return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            s.modelState = ModelState;
+            // If we got this far, something failed, redisplay form
             return Redirect("Pacijenti");
         }
         public IActionResult ObrisiPacijenta(string PacijentId)
--- B.U.Z/B.U.Z/ViewModels/PacijentiDodajVM.cs
+++ B.U.Z/B.U.Z/ViewModels/PacijentiDodajVM.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,18 @@ namespace B.U.Z.ViewModels
 {
     public class PacijentiDodajVM
     {
+        internal ModelStateDictionary modelState;
+
         public string PacijentId{ get; set; }
         public string Ime{ get; set; }
         public string Prezime{ get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "The password must contain at least one character, one number and one special character.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string PasswordHash { get; set; }
         public DateTime DatumRodjenja { get; set; }
         public string BrojTelefona{ get; set; }
         public int UkupnoPacijenata{ get; set; }
